0c6ade469c5dbc207deef72024ee0418ecc1d9cb
bird1217 <bird1217@gmail.com>
2016-07-11 21:28:50 +0800

    [測試紅燈]
    加入測試方法 [第一集買一本_第二集買一本_結帳金額為190元]
    
    Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元(綠燈)
    Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190(紅燈)

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShopping_Test.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShopping_Test.cs
@@ -27,5 +27,23 @@ namespace PotterShoppingCart.Tests
             var expected = 100;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void 第一集買一本_第二集買一本_結帳金額為190元()
+        {
+            //Arrange            
+            var shoppingCart = new ShopppingCart();
+            var book1 = new Book() { chapters = 1, Price = 100 };
+            var book2 = new Book() { chapters = 2, Price = 100 };
+            shoppingCart.Add(book1);
+            shoppingCart.Add(book2);
+
+            //Act
+            var actual = shoppingCart.CalculatePrice();
+
+            //Assert
+            var expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
